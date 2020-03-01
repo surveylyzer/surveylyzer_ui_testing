32b99605dc4cdca20a2f29f6ad41ace1957754ae
Bogumila Dubel <bogumila.dubel@namics.com>
2020-03-01 15:20:08 +0100

    - add serenity steps

--- src/test/java/stepdefinition/SmokeTestSteps.java
+++ src/test/java/stepdefinition/SmokeTestSteps.java
@@ -1,15 +1,18 @@
 package stepdefinition;
 
+import net.thucydides.core.annotations.Step;
 import pages.StartPage;
 
 public class SmokeTestSteps {
 
     private StartPage startPage;
 
+    @Step
     public void i_open_start_page() {
         startPage.open();
     }
 
+    @Step
     public void i_should_see_header_text(String title) {
         startPage.should_see_header_text(title);
     }
